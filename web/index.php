6ebf96e59b95ae93c849bbc998c94d6d841ae180
近澤徹大 <tetsuochikazawa11@gmail.com>
2015-12-25 19:06:57 +0900

    index.php chenge

--- web/index.php
+++ web/index.php
@@ -4,5 +4,27 @@ require('../vendor/autoload.php');
 
 echo "hello world";
 
+
+$conn = mysqli_connect('127.0.0.1' , 'root' , 'root');
+
+if($conn){
+  //データベースの選択
+  mysqli_select_db($conn , 'sample_db');
+  mysqli_query($conn , 'SET NAMES utf8');
+  //データベースから取り出しSQL文 1
+  $sql = 'SELECT message_id , message_title , message , user_name , entry_date FROM message_tb ORDER BY message_id';
+
+  //SQL文の実行
+  $query = mysqli_query($conn , $sql);
+}else{
+  echo "error";
+}
+
+
+e
+
+
+
+
 ?>
 
