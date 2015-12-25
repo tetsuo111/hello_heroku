5671e7a01113f16932f4d6ddd4e893178291b724
近澤徹大 <tetsuochikazawa11@gmail.com>
2015-12-25 20:56:51 +0900

    chenge

--- web/index.php
+++ web/index.php
@@ -5,19 +5,23 @@ require('../vendor/autoload.php');
 echo "hello world";
 
 
- $url = parse_url(getenv("mysql://b25c7548cad8b5:26989079@us-cdbr-iron-east-03.cleardb.net/heroku_3089e5be4c942aa?reconnect=true"));
+$conn = mysqli_connect('us-cdbr-iron-east-03.cleardb.net' , 'b25c7548cad8b5' , 'b25c7548cad8b5');
 
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
+if($conn){
+  //データベースの選択
+  mysqli_select_db($conn , 'sample_db');
+  mysqli_query($conn , 'SET NAMES utf8');
+  //データベースから取り出しSQL文 1
+  $sql = 'SELECT message_id , message_title , message , user_name , entry_date FROM message_tb ORDER BY message_id';
 
-    $link = mysqli_connect($server, $username, $password, $db);
-    $result = mysqli_query($link, "select * from user");
+  //SQL文の実行
+  $query = mysqli_query($conn , $sql);
+}else{
+  echo "error";
+}
 
-    while($user = mysqli_fetch_array($result)) {
-      echo $user['id'], " : ", $user['name'], "<br>";
-    }
+
+
 
 
 
