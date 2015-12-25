019f7bfdf026bdeb9373d8bcc4da8e495bc88e41
近澤徹大 <tetsuochikazawa11@gmail.com>
2015-12-25 21:16:24 +0900

    daizyoubu

--- web/index.php
+++ web/index.php
@@ -3,18 +3,17 @@
 require('../vendor/autoload.php');
 
 
+$host = 'us-cdbr-iron-east-03.cleardb.net';
+$db_user = 'b25c7548cad8b5';
+$db_pass = '26989079';
 
-$conn = mysqli_connect('us-cdbr-iron-east-03.cleardb.net' , 'b25c7548cad8b5' , 'b25c7548cad8b5');
+
+$conn = mysqli_connect($host , $db_user , $db_pass);
 
 if(!$conn){
-  mysqli_query($conn , 'SET NAMES utf8');
-  die('接続失敗です' .mysqli_error());
+  mysqli_query($conn ,'SET NAMES utf8' );
+  die('接続失敗'  .mysql_error());
 }
-
-
-mysqli_close($conn);
-
-
 
 ?>
 
