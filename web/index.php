2c1990671bcdf1628070d64aa2ba3ce0ccfcea88
近澤徹大 <tetsuochikazawa11@gmail.com>
2015-12-25 21:05:38 +0900

    文字化け

--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ require('../vendor/autoload.php');
 $conn = mysqli_connect('us-cdbr-iron-east-03.cleardb.net' , 'b25c7548cad8b5' , 'b25c7548cad8b5');
 
 if(!$conn){
+  mysqli_query($conn , 'SET NAMES utf8');
   die('接続失敗です' .mysqli_error());
 }
 
